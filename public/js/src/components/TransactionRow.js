dbedf56dc11af5fadbd588f509cd7a11ac07bb0f
Joseph William Hauger <joseph>
2019-08-07 17:57:51 -0500

    making components and such

---
+++ public/js/src/components/TransactionRow.js
@@ -0,0 +1,10 @@
+function TransactionRow(props){
+    return (
+<tr>
+    <td>{props.orderId}</td>
+    <td>{props.orderDate}</td>
+    <td>{props.orderTime}</td>
+    <td>{props.orderAmount}</td>
+</tr>
+    )
+}
